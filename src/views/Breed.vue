2155c3022028baa2be764e398eed4a90f6dcf5f5
Wael85 <dr.warl85@gmail.com>
2021-04-21 10:13:48 +0200

    add breed view

---
+++ src/views/Breed.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container v-for="n in 24 ">
+    <v-card height="300"> {{n}}</v-card>
+  </v-container>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'BreedView',
+  data: () => ({
+
+  }),
+});
+</script>
+
+<style scoped>
+
+</style>
